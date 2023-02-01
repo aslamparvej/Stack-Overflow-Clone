a51342062ddc32e83f7336e50dd0e9689d028677
Aslam Parvej <aslamparvej2@gmail.com>
2023-02-01 10:30:41 +0530

    Project Setup

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {BrowserRouter as Router} from "react-router-dom";
+
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+
+      </Router>
+    </div>
+     
+  );
+}
+
+export default App;
